9683b9350102762d347bed7260685a0729776796
BaoHaoYu <a450215151@outlook.com>
2020-05-23 15:30:03 +0800

    文章列表布局

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'gatsby';
 import './Article.scss';
 import { Meta, IMeta } from '~/components/Meta/Meta';
-
+import { Row, Col } from 'antd';
+import cn from 'classnames';
 interface IProps extends IMeta {
   title: string;
   excerpt: string;
@@ -22,22 +23,35 @@ const Article = ({
   categories,
   tags,
 }: IProps) => {
+  const list = true;
   return (
-    <div className={'Article'}>
-      {banner && (
-        <div className={'Article__banner'}>
-          <img src={banner} alt={title} />
-        </div>
-      )}
-      <h3 className={'Article__title'}>
-        <Link to={`/blog/${slug}`}>{title}</Link>
-      </h3>
-      <Meta tags={tags} categories={categories} date={date} timeToRead={timeToRead} />
-      <p className={'Article__excerpt'}>{description || excerpt}</p>
+    <div
+      className={cn('Article', {
+        'Article--list': list,
+      })}
+    >
+      <Row gutter={list ? 30 : 0}>
+        <Col span={list ? 8 : 24}>
+          {banner && (
+            <div className={'Article__banner'}>
+              <img src={banner} alt={title} />
+            </div>
+          )}
+        </Col>
+        <Col span={list ? 16 : 24}>
+          <h3 className={cn('Article__title', { h5: list })}>
+            <Link to={`/blog/${slug}`}>{title}</Link>
+          </h3>
+          <div className={'Article__meta'}>
+            <Meta tags={tags} categories={categories} date={date} timeToRead={timeToRead} />
+          </div>
+          <p className={'Article__excerpt'}>{description || excerpt}</p>
 
-      <Link className={'Article__read'} to={`/blog/${slug}`}>
-        阅读
-      </Link>
+          <Link className={'Article__read'} to={`/blog/${slug}`}>
+            阅读
+          </Link>
+        </Col>
+      </Row>
     </div>
   );
 };
